8d10e374c7d07a68c738a93aad003fb6e92410c7
RogerWater <zhangjin0205@mail.nwpu.edu.cn>
2024-06-28 15:29:57 +0800

    add urdf file of kuka with sliding joint

--- urdf_test.py
+++ urdf_test.py
@@ -23,35 +23,29 @@ if __name__ == "__main__":
     cube_visual = p.createVisualShape(p.GEOM_BOX, halfExtents=[6, 1, 0.125], rgbaColor=[1, 0, 0, 1])
     cubeID = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=cube_collision, baseVisualShapeIndex=cube_visual, basePosition=[0, 0, 0.3])
 
-    robotID = p.loadURDF("E:/Academic/Master/RL/pybullet_robot_planning/urdf/kuka.urdf", [0, 2, 0], [0, 0, 0, 1], globalScaling=2)
+    robotID = p.loadURDF("E:/Academic/Master/RL/pybullet_robot_planning/urdf/kuka_with_slide/kuka.urdf", [0, 2, 0], [0, 0, 0, 1], globalScaling=2)
 
     numJoints = p.getNumJoints(robotID)
     print("numJoints:", numJoints)
     jointPositions = [p.getJointState(robotID, i)[0] for i in range(numJoints)]
     print("jointPositions:", jointPositions)
 
-    endEffectorName = "lbr_iiwa_link_7"
-    endEffectorIndex = -1
+    slidingJointName = "sliding_joint"
+    slidingJointIndex = -1
     for jointIndex in range(numJoints):
         jointInfo = p.getJointInfo(robotID, jointIndex)
-        if jointInfo[12].decode("utf-8") == endEffectorName:
-            endEffectorIndex = jointIndex
+        if jointInfo[1].decode("utf-8") == slidingJointName:
+            slidingJointIndex = jointIndex
             break
+    print("slidingJointIndex:", slidingJointIndex)
 
-    if endEffectorName == -1:
-        print("End effector not found")
-    else:
-        print("End effector index:", endEffectorIndex)
+    initial_position = -6
+    p.resetJointState(robotID, slidingJointIndex, initial_position)
+    target_position = 12
+    p.setJointMotorControl2(bodyUniqueId=robotID, jointIndex=slidingJointIndex,
+                            controlMode=p.POSITION_CONTROL, targetPosition=target_position, force=500)
 
-    targetPositions = [0, -2, 2]
-    print("targetPositions:", targetPositions)
-    targetOrientation = p.getQuaternionFromEuler([0, 0, 0])
-    print("targetOrientation:", targetOrientation)
-    targetJointPositions = p.calculateInverseKinematics(robotID, endEffectorIndex, targetPositions, targetOrientation)
-    print("targetJointPositions:", targetJointPositions)
-    p.setJointMotorControlArray(robotID, range(numJoints), p.POSITION_CONTROL, targetJointPositions)
-
-    for i in range(100):
+    for i in range(1000):
         p.stepSimulation()
         time.sleep(1./10.)
         print("i:", i)
